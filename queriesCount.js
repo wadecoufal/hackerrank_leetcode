201ab30fc655ef97b713e9180f5c1de6e59c3580
Wade Coufal <wade.coufal@gmail.com>
2019-01-08 21:59:32 -0800

    Begin optimal solution for queriesCount

--- queriesCount.js
+++ queriesCount.js
@@ -1,8 +1,45 @@
 
-// q = [[1,1]]
 
-
+// works, but not optimal. The Object.values method and for loop within case 3 of the switch
+// statement is costly.
 function freqQuery(queries) {
+    const counts = {}; // { 1 => 2}
+    const results = []; // [0, 1]
+
+    queries.forEach(query => { // [3,2]
+        const val = query[1]; //
+        switch (query[0]) { //
+            case 1:
+                counts[val] ? counts[val] += 1 : counts[val] = 1;
+                break;
+            case 2:
+                counts[val] ? counts[val] -= 1 : null;
+                break;
+            case 3:
+                const vals = Object.values(counts); // [2]
+                let countFound = false;
+                for (let i = 0; i < vals.length; i++) {
+                    if (vals[i] === val) { // 2 === 2
+                        results.push(1);
+                        countFound = true;
+                        break;
+                    }
+                }
+                if (!countFound) {
+                    results.push(0);
+                }
+                break;
+        }
+    })
+
+    return results;
+
+}
+
+
+// more optimal solution (not yet working)
+
+function freqQuery2(queries) {
     const queryCounts = {}; // {5: 1, 6: 1, 10: 1}
     const counts = {}; // {1: 3}
     const results = []; //
